95b7c50a1877702f7f4ac74dcecd51f8cbd0bd13
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-02-07 02:32:49 +0600

    tour page image is added

--- src/pages/TourPlane.jsx
+++ src/pages/TourPlane.jsx
@@ -6,22 +6,22 @@ const TourPlane = () => {
         <div className='bg-slate-300'>
             <h1 className='text-2xl font-bold'> Tour Plane  </h1>
             <div className='max-w-xl  h-fullrounded-sm p-2 mx-auto hover:cursor-zoom-in' >
-                <img className='rounded-sm' src="/public/schedule/tour-1.png" alt="" />
+                <img className='rounded-sm' src="/schedule/tour-1.png" alt="" />
             </div>
             <div className='max-w-xl  h-fullrounded-sm p-2 mx-auto ' >
-                <img className='rounded-sm' src="/public/schedule/tour-2.png" alt="" />
+                <img className='rounded-sm' src="/schedule/tour-2.png" alt="" />
             </div>
             <div className='max-w-xl  h-fullrounded-sm p-2 mx-auto ' >
-                <img className='rounded-sm' src="/public/schedule/tour-3.png" alt="" />
+                <img className='rounded-sm' src="/schedule/tour-3.png" alt="" />
             </div>
             <div className='max-w-xl  h-fullrounded-sm p-2 mx-auto ' >
-                <img className='rounded-sm' src="/public/schedule/tour-4.png" alt="" />
+                <img className='rounded-sm' src="/schedule/tour-4.png" alt="" />
             </div>
             <div className='max-w-xl  h-fullrounded-sm p-2 mx-auto ' >
-                <img className='rounded-sm' src="/public/schedule/tour-5.png" alt="" />
+                <img className='rounded-sm' src="/schedule/tour-5.png" alt="" />
             </div>
             <div className='max-w-xl  h-fullrounded-sm p-2 mx-auto ' >
-                <img className='rounded-sm' src="/public/schedule/tour-6.png" alt="" />
+                <img className='rounded-sm' src="/schedule/tour-6.png" alt="" />
             </div>
 
         </div>
